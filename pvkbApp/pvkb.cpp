67eebadd28588ac6719edea2c12d7844c16cf19e
nmarks99 <nmarks@anl.gov>
2024-10-25 15:47:08 -0500

    basic trigging on arrow keys works!

--- pvkbApp/pvkb.cpp
+++ pvkbApp/pvkb.cpp
@@ -1,16 +1,18 @@
 #include <cctype>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <optional>
-#include <ncurses.h>
 #include <map>
 #include <tuple>
 #include <variant>
+#include <ncurses.h>
 
 #include <pva/client.h>
 #include <pv/caProvider.h>
 
-#include <toml++/toml.hpp>
+#include "toml++/toml.hpp"
+
 
 std::optional<char> to_key_char(const std::string_view str) {
     
@@ -42,15 +44,50 @@ std::optional<char> to_key_char(const std::string_view str) {
     }
 }
 
-// using VarType = std::variant<int, double, std::string>;
-// using MapVal = std::tuple<pvac::ClientChannel, VarType>;
-// std::map<char, MapVal> channel_map;
-//
-// std::map<char, MapVal> get_keybindings(toml::table tbl) {
-    // std::map<char, MapVal> channel_map;
-    // return channel_map;
-// }
-//
+using VarType = std::variant<int, double, std::string>;
+using TupleVal = std::tuple<pvac::ClientChannel, VarType>;
+
+std::map<char, TupleVal> get_keybindings(const toml::table &tbl, pvac::ClientProvider &provider) {
+    std::stringstream err_ss;
+    std::map<char, TupleVal> channel_map;
+
+    // get the IOC prefix
+    const std::optional<std::string> ioc_prefix = tbl["prefix"].value<std::string>();
+    if (not ioc_prefix.has_value()) {
+	err_ss.clear();
+	err_ss << "IOC prefix is required" << std::endl;
+	throw std::runtime_error(err_ss.str());
+    }
+
+    // get all the keybindings and construct the map
+    for (const auto &[key, value] : *tbl["keybindings"].as_table()) {
+	const auto keybind = *value.as_table();
+	const std::optional<std::string> pv_name = keybind["pv"].value<std::string>();
+	if (not pv_name.has_value()) {
+	    err_ss.clear();
+	    err_ss << "Missing or invalid PV name in keybind " << "'" << key << "'" << std::endl;
+	    throw std::runtime_error(err_ss.str());
+	}
+
+	// TODO: support multiple data types
+	const std::optional<int> pv_val = keybind["value"].value_or(1);
+
+	std::optional<char> key_char = to_key_char(key);
+	if (not key_char.has_value()) {
+	    err_ss.clear();
+	    err_ss << "Invalid key " << "'" << key << "'" << std::endl;
+	    throw std::runtime_error(err_ss.str());
+	}
+
+	// connect the channel to the PV
+	pvac::ClientChannel channel(provider.connect(ioc_prefix.value() + pv_name.value()));
+
+	// add the key char, channel, and PV value to the map
+	channel_map[key_char.value()] = std::make_tuple(channel, pv_val.value());
+
+    }
+    return channel_map;
+}
 
 
 int main(int argc, char *argv[]) {
@@ -60,75 +97,38 @@ int main(int argc, char *argv[]) {
 	return 1;
     }
 
+    // parse the toml file into a toml::table
     toml::table tbl;
     try {
         tbl = toml::parse_file(argv[1]);
-        // std::cout << tbl << "\n";
     } catch (const toml::parse_error& err) {
         std::cerr << "Parsing failed:\n" << err << "\n";
         return 1;
     }
-
-    // get the IOC prefix
-    const std::optional<std::string> ioc_prefix = tbl["prefix"].value<std::string>();
-    if (not ioc_prefix.has_value()) {
-	std::cerr << "IOC prefix is required" << std::endl;
-	return 1;
-    }
-
-    // get the provider (channel access or pv access), default ca
-    const std::optional<std::string> provider_name = tbl["provider"].value_or("ca");
-
-    std::cout << "using IOC prefix " << ioc_prefix.value() << std::endl;
-    std::cout << "using provider " << provider_name.value() << std::endl;
-   
+    
+    // Get the provider "ca" or "pva", default ca
     epics::pvAccess::ca::CAClientFactory::start();
+    const std::optional<std::string> provider_name = tbl["provider"].value_or("ca");
     pvac::ClientProvider provider(provider_name.value());
-   
-    // store information in a map: key char -> (PVChannel, pv value)
-    using VarType = std::variant<int, double, std::string>;
-    using MapVal = std::tuple<pvac::ClientChannel, VarType>;
-    std::map<char, MapVal> channel_map;
+    
+    // Get the mapping key_char -> (pv channel, pv value to write)
+    std::map<char, TupleVal> channel_map = get_keybindings(tbl, provider);
 
-    // get the keybindings
-    for (const auto &[key, value] : *tbl["keybindings"].as_table()) {
-	const auto keybind = *value.as_table();
-
-	const std::optional<std::string> pv_name = keybind["pv"].value<std::string>();
-	if (not pv_name.has_value()) {
-	    std::cerr << "Missing or invalid PV name in keybind " << key << std::endl;
-	    return 1;
+    // initialize ncurses
+    initscr();
+    keypad(stdscr, TRUE);
+    noecho();
+    
+    while (true) {
+	int ch = getch();
+	if (ch == 'q') {
+	    break;
 	}
-
-	// TODO: support multiple data types
-	const std::optional<int> pv_val = keybind["value"].value<int>();
-	if (not pv_val.has_value()) {
-	    std::cerr << "Missing or invalid value in keybind " << key << std::endl;
-	    return 1;
-	}
-
-	std::optional<char> key_char = to_key_char(key);
-	if (not key_char.has_value()) {
-	    std::cerr << "Invalid key " << key << std::endl;
-	    return 1;
-	}
-
-	std::cout << key_char.value() << " : " << pv_name.value() << " = " << pv_val.value() << std::endl;
-
-	// connect the channel to the PV
-	pvac::ClientChannel channel(provider.connect(ioc_prefix.value() + pv_name.value()));
-
-	// add the key char, channel, and PV value to the map
-	channel_map[key_char.value()] = std::make_tuple(channel, pv_val.value());
-
+	const auto val = std::get<int>(std::get<1>(channel_map[ch]));
+	std::get<0>(channel_map[ch]).put().set("value", val).exec();
+	refresh();
     }
-
-    for (const auto &[key, value] : channel_map) {
-	pvac::ClientChannel channel = std::get<0>(value);
-	std::cout << channel.get() << std::endl;
-    }
-
-    // TODO: match char from getch() to keys in channel_map,
+    endwin();
 
     return 0;
 }
